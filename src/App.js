cfb4f9fcf4d90f5ed2248236843adab131652131
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2022-04-06 23:55:16 +0200

    previous-lab-scaffolding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes, Navigate } from 'react-router';
 import './App.css';
+import Home from "./components/Home"
+import Beers from "./components/Beers"
+import BeerDetails from "./components/BeerDetails"
+import RandomBeer from "./components/RandomBeer"
+import NewBeer from "./components/NewBeer"
+import NavBar from "./components/NavBar"
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/beers" element={<Beers />} />
+        <Route path="/beers/:id" element={<BeerDetails />} />
+        <Route path="/random" element={<RandomBeer />} />
+        <Route path="/new" element={<NewBeer />} />
+        <Route path="*" element={<Navigate to="/home" replace/>} />
+      </Routes>
     </div>
   );
 }
